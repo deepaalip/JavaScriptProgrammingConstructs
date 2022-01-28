0ad52c0c32d2f6298d3fb4b32cae94b15eff917e
deepali <deepaalipatil@gmail.com>
2022-01-28 21:50:51 +0530

    ADDED:program for printing word for single digit

---
+++ SelectionPracticeProblemsCaseState/SingleDigitWord.js
@@ -0,0 +1,66 @@
+
+let num = Math.floor(Math.random()*10);
+switch(num){
+ case 0 :
+    if(num == 0){
+        word = "ZERO";
+    }
+     
+ break; 
+
+ case 1:
+     if(num == 1){
+        word = "ONE";
+    }
+     
+ break; 
+ case 2 :
+    if(num == 2){
+        word = "TWO";
+     }
+    
+ break; 
+ case 3 :
+    if(num == 3){
+        word = "THREE";
+     }
+     
+ break; 
+ case 4 :
+    if(num == 4){
+        word = "FOUR";
+     }
+ break; 
+ case 5 :
+    if(num == 5){
+        word = "FIVE";
+     }
+ break; 
+ case 6 :
+    if(num == 6){
+        word = "SIX";
+     }
+ break; 
+ case 7 :
+    if(num == 7){
+        word = "SEVEN";
+     }
+break; 
+case 8 :
+    if(num == 8){
+        word = "EIGHT";
+     }
+break; 
+case 9 :
+    if(num == 9){
+        word = "NINE"; 
+     }
+break; 
+default :
+  console.log("enter valid option") ;
+   
+} 
+ 
+
+console.log(word);
+
