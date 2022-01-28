0d32911f15e99ec75518a5895e6320f687ce5072
deepali <deepaalipatil@gmail.com>
2022-01-28 21:50:05 +0530

    ADDED:program for printing units for numbers

---
+++ SelectionPracticeProblemsCaseState/NumbersUnits.js
@@ -0,0 +1,31 @@
+let num = 100;
+switch(num){
+   case 1:
+     if(num == 1){
+     console.log("UNIT");
+   }
+   break;
+   case 10:
+    if(num == 10){
+   console.log("TEN");
+  }
+  break;
+  case 100:
+   if(num == 100){
+   console.log("HUNDRED");
+  }
+  break;
+  case 1000:
+   if(num == 1000){
+   console.log("THOUSAND");
+  }
+   break;
+   case 10000:
+  if(num == 10000){
+	console.log("TEN THOUSAND");
+  }
+  break;
+  default:
+	console.log("Enter valid number.");
+
+}
