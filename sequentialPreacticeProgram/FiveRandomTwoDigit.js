9b0a776c2a7e7a46240eee12512b82a49a76d5d9
deepali <deepaalipatil@gmail.com>
2022-01-28 21:30:53 +0530

    ADDED:Random function for getting two digit

---
+++ sequentialPreacticeProgram/FiveRandomTwoDigit.js
@@ -0,0 +1,9 @@
+let digit1 = Math.floor(Math.random() * 100);
+let digit2 = Math.floor(Math.random() * 100);
+let digit3 = Math.floor(Math.random() * 100);
+let digit4 = Math.floor(Math.random() * 100);
+let digit5 = Math.floor(Math.random() * 100);
+
+let sum = digit1+digit2+digit3+digit4+digit5;
+let average = sum/5;
+console.log("Sum of all random digits is :"+ sum +  " Average is: "+average);
