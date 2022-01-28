a2f1a672206ddc71d32c508287447ed5857f6a81
deepali <deepaalipatil@gmail.com>
2022-01-28 21:32:25 +0530

    ADDED:Random function for adding two random number

---
+++ sequentialPreacticeProgram/AddTwoRandomNumber.js
@@ -0,0 +1,5 @@
+let random1 = Math.floor(Math.random()*6+1);
+let random2 = Math.floor(Math.random()*6+1);
+console.log("Random 1 : " + random1);
+console.log("Random 2 : " + random2);
+console.log("Addition of two random numbers is: " + (random1+random2));
