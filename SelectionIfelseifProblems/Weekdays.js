7ac413b7845cb004beaa89dc328a4c5d366b6a1f
deepali <deepaalipatil@gmail.com>
2022-01-28 21:44:31 +0530

    ADDED:program for printing weekdays

---
+++ SelectionIfelseifProblems/Weekdays.js
@@ -0,0 +1,24 @@
+let num = Math.floor(Math.random()*10);
+
+if(num == 0){
+	console.log("SUNDAY");
+}
+else if(num == 1){
+	console.log("MONDAY");
+}
+else if(num == 2){
+   console.log("TUESDAY");
+}
+else if(num == 3){
+   console.log("WEDNESDAY");
+}
+else if(num == 4){
+   console.log("THURSDAY");
+}
+else if(num == 5){
+   console.log("FRIDAY");
+}
+else {
+   console.log("SATURDAY");
+}
+
