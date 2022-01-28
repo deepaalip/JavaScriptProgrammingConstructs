d2ee9f702eb29e80f41d9732e9d43a25c2eba016
deepali <deepaalipatil@gmail.com>
2022-01-28 21:42:37 +0530

    ADDED:program for display units for number

---
+++ SelectionIfelseifProblems/DisplayNumberUnits.js
@@ -0,0 +1,20 @@
+let num = 100;
+
+if(num == 1){
+   console.log("UNIT");
+}
+else if(num == 10){
+   console.log("TEN");
+}
+else if(num == 100){
+   console.log("HUNDRED");
+}
+else if(num == 1000){
+   console.log("THOUSAND");
+}
+else if(num == 10000){
+	console.log("TEN THOUSAND");
+}
+else {
+	console.log("Enter valid number.");
+}
